ec6bc092eeac026c3940acc1b7c2271479153448
pedrofarr <pedrinifarrusini@gmail.com>
2025-09-01 09:38:12 -0300

    Add files via upload

--- tp3-pedrofarr-main/in_string.py
+++ tp3-pedrofarr-main/in_string.py
@@ -1,14 +1,6 @@
 def check_vowels():
-    nombre = input("Ingresa un nombre: ")
-    nombre = nombre.lower()
-    contiene_a = nombre.count('a') > 0
-    contiene_e = nombre.count('e') > 0
-    contiene_i = nombre.count('i') > 0
-    contiene_o = nombre.count('o') > 0
-    contiene_u = nombre.count('u') > 0
-    print(f"Contiene a: {contiene_a}")
-    print(f"Contiene e: {contiene_e}")
-    print(f"Contiene i: {contiene_i}")
-    print(f"Contiene o: {contiene_o}")
-    print(f"Contiene u: {contiene_u}")
-check_vowels()
+    # Código a implementar utilizando input.
+
+
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- tp3-pedrofarr-main/slice_advanced.py
+++ tp3-pedrofarr-main/slice_advanced.py
@@ -1,5 +1,5 @@
 def slice_advanced():
-    texto = input()
-    resultado = texto[4::2]
-    print(resultado)
-slice_advanced()
+    # Código a implementar utilizando input.
+
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
