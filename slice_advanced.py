580eb2faba8fad1f9fbdedc7f0819fa860a0b021
pedrofarr <pedrinifarrusini@gmail.com>
2025-12-04 19:41:04 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,18 +1,7 @@
-# Ejercicio 2 - Operaciones de slicing
-def ejercicio2():
-    texto = "Awesome"
-    
-    # Primera operación
-    resultado1 = texto[0:3].lower()
-    print(resultado1)
-    
-    # Segunda operación
-    resultado2 = texto[2:5].lower()
-    print(resultado2)
-    
-    # Tercera operación
-    resultado3 = (texto[0:4] + texto[-3:]).lower()
-    print(resultado3)
+def slice_advanced():
+    texto = input()
+    resultado = texto[4::2]
+    print(resultado)
+# slice_advanced()
 
-if _name_ == "_main_":
-    ejercicio2()
+# Pedro Farrus
--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,12 @@
-# Ejercicio 3 - Slicing con paso
-def ejercicio3():
-    texto = input()
-    resultado = texto[4::2]
-    print(resultado)
+def slice_simple():
+    texto = "Awesome"
+    primeras_tres = texto[:3].lower()
+    media_tres = texto[2:5].lower()
+    primera_a_cuarta = texto[:].lower()
+    
+    print(primeras_tres)
+    print(media_tres)
+    print(primera_a_cuarta)
+# slice_simple()
 
-if _name_ == "_main_":
-    ejercicio3()
+# Pedro Farrus
